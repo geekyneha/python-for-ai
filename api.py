975512a2a13f7886c770bc0f386dc46d96dda609
geekyneha <nehatualea@gmail.com>
2026-06-28 15:04:32 +0530

    working with api

--- api.py
+++ api.py
@@ -1,17 +1,21 @@
 import requests
 
 # We need coordinates to get weather data
-latitude = 48.85   # Paris latitude
-longitude = 2.35   # Paris longitude
 
+def get_weather(latitude, longitude):
 # Build the API URL with our parameters
-url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current=temperature_2m"
+    url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current=temperature_2m"
 
-response = requests.get(url)
-data = response.json()
-print(response.status_code)
-
-print(data)
+    response = requests.get(url)
+    data = response.json()
+    return data['current']['temperature_2m']
 
 
+paris_weather = get_weather(48.8566, 2.3522)
+london_weather = get_weather(51.5074, -0.1278)
+austin_weather = get_weather(30.2672, -97.7431)
+
+print(f"paris: {paris_weather}°C")
+print(f"london: {london_weather}°C")
+print(f"austin: {austin_weather}°C")
 
